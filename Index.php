0fafb6ed509e37b900e22b713a123d65c69c5a1a
romunroe7 <rohaya.wawaeni007@gmail.com>
2026-01-29 09:57:06 +0100

    Kod innan byte till max kod 2

--- Index.php
+++ Index.php
@@ -1,1 +1,48 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <div class="navbar">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Hello... user!</a>
+        </li>
+        <li class="nav-item">
+            <button type="button" class="btn btn-light">Log out</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+
+    <div class="addlinks">
+        <p>In this box you view links</p> 
+    </div>
+
+    <div class="todolist">
+    </div>
+
+    <div class="timer">
+    </div>
+    
+</body>
+</html>
